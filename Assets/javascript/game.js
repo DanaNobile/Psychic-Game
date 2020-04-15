2edb3609308cb5a3b882d37d30f9baa550b8aedb
Dana Nobile <danagnobile@gmail.com>
2020-04-15 09:29:01 -0400

    Set up files and folders, initial html and variables

---
+++ Assets/javascript/game.js
@@ -0,0 +1,11 @@
+
+
+
+var userguess [];
+var wins = [];
+var loses = [];
+var remaining = [];
+var guesses = [];
+
+document.onkeyup = function (event){
+    var userGuess = event.key; // user guess
