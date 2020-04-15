e542b5103e98eb4569e4d8df34c93eae725be536
Dana Nobile <danagnobile@gmail.com>
2020-04-15 15:43:29 -0400

    Created computer and user key function; created updates to wins and losses with if else statements

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -1,12 +1,60 @@
 
+// var secretLetter = ""; //computer 
+// var guesses = [];
 
-var userguess =[];
-var wins = [];
-var loses = [];
-var remaining = 9;
-var guesses = [];
-var secretLetter = "";
-var alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+// Creates an array that lists out all of the options in the alphabet
+var computerChoices = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',]
+
+
+// Creating variables to hold the number of wins, losses, and remaining. 
+var wins = 0;
+var losses = 0;
+var remaining = 10;
+
+// Create variables that hold references to the places in the HTML where we want to display things.
+
+var winsNumber = document.getElementById("wins");
+var lossesNumber = document.getElementById("losses");
+var remainingNumber = document.getElementById("remaining");
+var guessesSoFar = document.getElementById("guessesSoFar");
+
+// This function is run whenever the user presses a key.
+
+document.onkeyup = function(event){
+// Determines which key was pressed.
+var userGuess = event.key;
+
+// Randomly chooses a choice from the computerChoices array. This is the Computer's letter.
+var computerLetter = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+//This logic determines the outcome of the key pressed (win/loss)=, and increments the appropriate number
+
+if(userGuess === computerLetter){
+    wins++}
+
+if(userGuess != computerLetter){
+    losses++;
+    remaining--;
+    //include listing out guessesSoFar 
+}
+
+//Display wins/losses/guesses reamining
+winsNumber.textContent = wins;
+lossesNumber.textContent = + losses;
+
+
+
+
+console.log(userGuess);
+console.log(computerLetter);
+}
+
+
+
+
+
+
 
 
 
@@ -16,18 +64,18 @@ var alphabet = "abcdefghijklmnopqrstuvwxyz"
   
   // Function to generate random number  
  
-var secretLetter = "";
-var alphabet = "abcdefghijklmnopqrstuvwxyz";
-function getRandomLetter() {
-  return alphabet[Math.floor(Math.random() * alphabet.length)];
-}
-var randomLetter;
+// var secretLetter = "";
+// var alphabet = "abcdefghijklmnopqrstuvwxyz";
+// function getRandomLetter() {
+//   return alphabet[Math.floor(Math.random() * alphabet.length)];
+// }
+// var randomLetter;
 
-while (secretLetter.length < 1) {
-  randomLetter = getRandomLetter();
-  secretLetter += randomLetter;
-} 
-console.log(secretLetter);         
+// while (secretLetter.length < 1) {
+//   randomLetter = getRandomLetter();
+//   secretLetter += randomLetter;
+// } 
+// console.log(secretLetter);         
     
 
 
