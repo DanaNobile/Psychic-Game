7bd52b6bf39530e595ce21985978a05830e4e34c
Dana Nobile <danagnobile@gmail.com>
2020-04-15 11:50:22 -0400

    Creaed function to generate randome letter and console log letter

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -1,11 +1,77 @@
 
 
-
-var userguess [];
+var userguess =[];
 var wins = [];
 var loses = [];
-var remaining = [];
+var remaining = 9;
 var guesses = [];
+var secretLetter = "";
+var alphabet = "abcdefghijklmnopqrstuvwxyz"
 
-document.onkeyup = function (event){
-    var userGuess = event.key; // user guess
+
+
+
+//Declarations: 
+
+  
+  // Function to generate random number  
+ 
+var secretLetter = "";
+var alphabet = "abcdefghijklmnopqrstuvwxyz";
+function getRandomLetter() {
+  return alphabet[Math.floor(Math.random() * alphabet.length)];
+}
+var randomLetter;
+
+while (secretLetter.length < 1) {
+  randomLetter = getRandomLetter();
+  secretLetter += randomLetter;
+} 
+console.log(secretLetter);         
+    
+
+
+
+// Function Calls:
+
+// functionName()
+
+
+
+
+
+
+
+
+
+
+
+
+// Logging number of remaining guesses
+// for (i=0; i<9; i++){
+//     console.log(i)
+// }
+
+
+// document.onkeyup = function (event){
+//     var userGuess = event.key; // user guess
+// }
+
+
+
+
+// 1. Computer needs to assign letter
+// 2. Gather userguess
+// 3. Check for a match
+// 4. Compare value of computer letter to userGuess
+      // if equal increase win
+      // if not equal increase loss
+// 5. decrease guesses left
+// 6. populate letters guessed so far
+
+
+
+
+
+
+// .push is an element to prompt an array}
